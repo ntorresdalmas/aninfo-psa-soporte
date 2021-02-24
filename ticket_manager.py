57697e5854449cc057375ea41a2da566146c49a1
ntorresdalmas <ntorresdalmas@fi.uba.ar>
2021-02-24 17:59:26 -0300

    editar edit_ticket() y create_ticket()

--- ticket_manager.py
+++ ticket_manager.py
@@ -1,32 +1,28 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from ticket import Ticket
 from project import Project
 import requests
 import db
 
 
-ALLOWED_STATES = ['abierto', 'en progreso soporte', 'escalado en ingeneria', 'esperando respuesta del cliente', 'resuelto']
+ALLOWED_STATES = ['en progreso soporte', 'escalado en ingeneria', 'esperando respuesta del cliente', 'resuelto'] #'abierto'
 ALLOWED_TICKET_TYPES = ['consulta', 'error']
-PRIORITY_AND_DAYS_TO_COMPLETE = {1 : 7, 2 : 90, 3 : 180, 4 : 365}   #{prioridad: maxima_cantidad_de_dias_limite}
+PRIORITY_AND_DAYS_TO_COMPLETE = {1: 7, 2: 90, 3: 180, 4: 365}   #{prioridad: maxima_cantidad_de_dias_limite}
 
 
 def create_ticket(content):
+    content['creation_date'] = datetime.now()
+    content['limit_date'] = timedelta(days=PRIORITY_AND_DAYS_TO_COMPLETE[content['priority']]) + content['creation_date']
+    content['status'] = 'abierto'
+    #TODO: recibir project_id sacandolo del task desde el FE.
     ticket = Ticket(**content)
     db.save_ticket(vars(ticket))
 
 
 def edit_ticket(content):
     _id = content.pop('ticket_id', None)
-    #TODO: escribir piola
-    #if ticket.priority:
-    #    content['limit_date'] = PRIORITY_AND_DAYS_TO_COMPLETE[ticket.priority] + ticket.creation_date
-    #
-    #if ticket.task_id:
-    #    # pegarle a la API de get_tareas y get_proyectos y obtener de la tarea el recurso_asignado y codigoProyecto
-    #    tareas = get_tareas()
-    #    tarea = tareas[ticket.task_id]
-    #    content['project_id'] = tarea.recursoAsignado
-    #    content['resource_id'] = tarea.codigoProyecto
+    if content['priority']:
+        content['limit_date'] = timedelta(days=PRIORITY_AND_DAYS_TO_COMPLETE[content['priority']]) + get_ticket_by_id(_id)['creation_date']
 
     db.edit_ticket(_id, content)
 
