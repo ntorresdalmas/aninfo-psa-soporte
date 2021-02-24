81baea5ee0e4745855902e8d67d325e68d2ff80b
Martin Rosas <martin.rosas@despegar.com>
2021-02-23 21:53:08 -0300

    added flask cors

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from ticket import Ticket
 import ticket_manager
 import json
 
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
 app.config["DEBUG"] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['GET'])
@@ -14,6 +18,7 @@ def home():
 
 
 @app.route('/tickets_main_data', methods=['POST'])
+@cross_origin()
 def get_all_tickets_main_data():
     """
     Exclusive use for front app
@@ -23,6 +28,7 @@ def get_all_tickets_main_data():
 
 
 @app.route('/<project_id>/tickets', methods=['GET'])
+@cross_origin()
 def get_tickets(project_id):
     """
     only shows the ones with state != resuelto
@@ -31,11 +37,13 @@ def get_tickets(project_id):
 
 
 @app.route('/tickets', methods=["GET"])
+@cross_origin()
 def get_all_tickets():
     return json.dumps(ticket_manager.get_all_tickets())
 
 
 @app.route('/<project_id>/createTicket', methods=['POST'])
+@cross_origin()
 def create_ticket(project_id):
     content = request.json
     ticket_manager.create_ticket(content)
@@ -44,6 +52,7 @@ def create_ticket(project_id):
 
 
 @app.route('/<project_id>/editTicket', methods=['POST'])
+@cross_origin()
 def edit_ticket(project_id):
     content = request.json
     ticket_manager.edit_ticket(content)
