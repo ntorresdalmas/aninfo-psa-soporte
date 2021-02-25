fcd84b6de60bb513bdea5e06ff165346d137c11b
Martin Rosas <martin.rosas@despegar.com>
2021-02-25 18:05:32 -0300

    changed datetime string format

--- ticket.py
+++ ticket.py
@@ -18,7 +18,7 @@ class Ticket:
 
     def as_dict(self):
         ticket_as_dict = vars(self)
-        ticket_as_dict["creation_date"] = str(ticket_as_dict["creation_date"])
-        ticket_as_dict["limit_date"] = str(ticket_as_dict["limit_date"])
+        ticket_as_dict["creation_date"] = datetime.strftime(ticket_as_dict["creation_date"], "%Y-%m-%d %H:%M:%S")
+        ticket_as_dict["limit_date"] = datetime.strftime(ticket_as_dict["limit_date"], "%Y-%m-%d %H:%M:%S")
         return ticket_as_dict
 
